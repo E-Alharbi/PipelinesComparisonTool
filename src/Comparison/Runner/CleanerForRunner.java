ae5910051b9b625422966962a8f377078d580f5b
Emad Alharbi <emra500@york.ac.uk>
2019-10-06 11:27:31 +0100

    Fixed a bug

--- src/Comparison/Runner/CleanerForRunner.java
+++ src/Comparison/Runner/CleanerForRunner.java
@@ -17,32 +17,43 @@ public class CleanerForRunner {
 		// TODO Auto-generated method stub
 
 
-	File [] PDBS= new File(RunningPram.PDBsDir).listFiles();
-	File [] Logs= new File(RunningPram.LogsDir).listFiles();
-String Script="";
-	for(File Log : Logs) {
-		String CaseName=Log.getName().replaceAll("."+FilenameUtils.getExtension(Log.getName()),"");	
-		Script+="rm J"+CaseName+".sh \n";
-		boolean found=false;
-		String PDBName="";
-for(File PDB : PDBS) {
-	String PDBID=PDB.getName().replaceAll("."+FilenameUtils.getExtension(PDB.getName()),"");	
-	PDBName=PDB.getName();
-	if(CaseName.equals(PDBID)) {
-		found=true;
-	}
-}
-if(found==false) {
-	Script+="rm "+RunningPram.LogsDir+"/"+Log.getName()+"\n";
-	Script+="rm "+RunningPram.IntermediateLogs+"/"+Log.getName()+"\n";
-	Script+="rm "+RunningPram.IntermediatePDBs+"/"+PDBName+"\n";
-}
-	}
+	File [] PDBS= new File(RunningParameter.PDBsDir).listFiles();
+	File [] Logs= new File(RunningParameter.LogsDir).listFiles();
+String Script=CheckWhichPDBIsBuilt(Logs,PDBS)+"\n";
+
+File [] InterLogs= new File(RunningParameter.IntermediateLogs).listFiles();
+ Script+=CheckWhichPDBIsBuilt(InterLogs,PDBS)+"\n";
 	Script+="rm *.sh.*"+"\n";
 	Script+="rm Qsub.sh"+"\n";
 	Script+="rm ProcessedFilesNames*"+"\n";
 	new Preparer().WriteTxtFile("Cleaner.sh", Script);
 	
+	}
+	
+	String CheckWhichPDBIsBuilt(File [] Logs,File [] PDBS) {
+		String Script="";
+		for(File Log : Logs) {
+			String CaseName=Log.getName().replaceAll("."+FilenameUtils.getExtension(Log.getName()),"");	
+			Script+="rm J"+CaseName+".sh \n";
+			boolean found=false;
+			
+			
+	for(File PDB : PDBS) {
+		String PDBID=PDB.getName().replaceAll("."+FilenameUtils.getExtension(PDB.getName()),"");	
+		
+		
+		if(CaseName.equals(PDBID)) {
+			found=true;
+		}
+	}
+	if(found==false) {
+		
+		Script+="rm "+RunningParameter.LogsDir+"/"+Log.getName()+"\n";
+		Script+="rm "+RunningParameter.IntermediateLogs+"/"+Log.getName()+"\n";
+		Script+="rm "+RunningParameter.IntermediatePDBs+"/"+CaseName+".pdb\n";
+	}
+		}
+		return Script;
 	}
 
 }
