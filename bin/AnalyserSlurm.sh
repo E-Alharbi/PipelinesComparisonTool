a1df320e3c2586eb7aba24e43bf947ad5241696f
Emad Alharbi <emra500@york.ac.uk>
2019-10-06 11:22:35 +0100

    Removed extra info

--- bin/AnalyserSlurm.sh
+++ bin/AnalyserSlurm.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=Emadjob          # Job name
 #SBATCH --time=48:00:00                # Time limit hrs:min:sec
-#SBATCH --mem=2000                     # Total memory limit
-#SBATCH -o logs/log_emad_o     # Standard output and error log
-#SBATCH -e logs/log_emad_e     # Standard output and error log
-##SBATCH --mail-type=END,FAIL         # Mail events (NONE, BEGIN, END, FAIL, ALL)
-##SBATCH --mail-user=emra500@york.ac.uk   # Where to send mail	
-##SBATCH --ntasks-per-node=8            # How many tasks on each node
-#SBATCH --account=CS-MPMSEDM-2018
+#SBATCH --mem=10000                     # Total memory limit
+&ccp4moduleload&
+&phenixmoduleload&
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:&ccp4&/lib
 export LD_LIBRARY_PATH
 export MALLOC_ARENA_MAX=4
