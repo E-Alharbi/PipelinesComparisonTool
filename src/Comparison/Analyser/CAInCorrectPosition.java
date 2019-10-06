6b67a8bc112906c3cb394e5c31370bd3f49c39f7
Emad Alharbi <emra500@york.ac.uk>
2019-10-06 12:53:26 +0100

    Completeness from CA position

---
+++ src/Comparison/Analyser/CAInCorrectPosition.java
@@ -0,0 +1,50 @@
+package Comparison.Analyser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Vector;
+
+public class CAInCorrectPosition {
+
+	public  void UpdateCom(File excel) throws FileNotFoundException, IOException {
+		// TODO Auto-generated method stub
+
+		
+		ExcelLoader f = new ExcelLoader();
+		Vector<ExcelContents> Pipeline1Excel = f.ReadExcel(excel.getAbsolutePath());
+		DecimalFormat df = new DecimalFormat("#.##");
+		df.setRoundingMode(RoundingMode.HALF_UP);
+		System.out.print(new CAInCorrectPosition().IsCalculatedFromSeq(Pipeline1Excel));
+		for(int i=0 ; i <Pipeline1Excel.size();++i ) {
+			
+		if(Pipeline1Excel.get(i).BuiltPDB.equals("T")) {	
+		String Completeness= df.format((BigDecimal.valueOf((Double.parseDouble(Pipeline1Excel.get(i).NumberOfAtomsInSecondNotInFirst) * 100.00)/Double.parseDouble(Pipeline1Excel.get(i).NumberofAtomsinFirstPDB)) ));
+		
+		
+		Pipeline1Excel.get(i).Completeness=Completeness;
+		}
+		}
+		excel.delete();
+		new ExcelSheet().FillInExcel(Pipeline1Excel, excel.getAbsolutePath());
+		
+	}
+	public boolean IsCalculatedFromSeq(Vector<ExcelContents> Pipeline1Excel) {
+		
+	
+		for(int i=0 ; i <Pipeline1Excel.size();++i ) {
+			if(Pipeline1Excel.get(i).BuiltPDB.equals("T")) {
+			DecimalFormat df = new DecimalFormat("#.##");
+			df.setRoundingMode(RoundingMode.HALF_UP);
+			String Completeness= df.format((BigDecimal.valueOf((Double.parseDouble(Pipeline1Excel.get(i).NumberOfAtomsInSecondNotInFirst) * 100.00)/Double.parseDouble(Pipeline1Excel.get(i).NumberofAtomsinFirstPDB)) ));
+if(Pipeline1Excel.get(i).Completeness.equals(Completeness))
+	return false;
+		}
+	}
+		return true;
+	}
+
+}
