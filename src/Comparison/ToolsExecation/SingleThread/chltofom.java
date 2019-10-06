62055a2dc81b8236a4071d92514e50eec126ad82
Emad Alharbi <emra500@york.ac.uk>
2019-10-06 11:26:57 +0100

    changed class name

--- src/Comparison/ToolsExecation/SingleThread/chltofom.java
+++ src/Comparison/ToolsExecation/SingleThread/chltofom.java
@@ -13,7 +13,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 
 import Comparison.Runner.RunComparison;
-import Comparison.Runner.RunningPram;
+import Comparison.Runner.RunningParameter;
 
 public class chltofom {
 
