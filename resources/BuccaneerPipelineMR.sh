7198fb47775230371b0ee6dc0c67e7c727ed3f6b
Emad Alharbi <emra500@york.ac.uk>
2020-04-04 02:38:06 +0100

    added mr-model-seed

--- resources/BuccaneerPipelineMR.sh
+++ resources/BuccaneerPipelineMR.sh
@@ -3,4 +3,4 @@
 #$ -l h_vmem=2G
 #$ -l h_rt=48:00:00
 cd @5
-python @1 -mtzin @2 -seqin  @3 -colin-fo FP,SIGFP -colin-hl model.HLA,model.HLB,model.HLC,model.HLD @6 -buccaneer-anisotropy-correction -buccaneer-fast -buccaneer-keyword verbose 5 -cycles @4 @7 -pdbin-mr @8 -refmac-mlhl false @9   
+python @1 -mtzin @2 -seqin  @3 -colin-fo FP,SIGFP -colin-hl model.HLA,model.HLB,model.HLC,model.HLD @6 -buccaneer-anisotropy-correction -buccaneer-fast -buccaneer-keyword verbose 5 -cycles @4 @7 -pdbin-mr @8 -refmac-mlhl false @9 -buccaneer-keyword mr-model-seed   
