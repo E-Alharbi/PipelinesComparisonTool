92684fca5564e34e8aea7a617b96e42719392017
Emad Alharbi <emra500@york.ac.uk>
2020-04-04 02:44:30 +0100

    added an option to use the resolution from Refmac

--- src/Comparison/Analyser/ResolutionUpdater.java
+++ src/Comparison/Analyser/ResolutionUpdater.java
@@ -7,9 +7,13 @@ package Comparison.Analyser;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Vector;
 
 import org.apache.commons.io.FileUtils;
+
+import Comparison.Runner.RunningParameter;
 
 public class ResolutionUpdater {
 
@@ -22,6 +26,8 @@ public static void main(String[] args) throws Exception{
 	public  void Update(String Path) throws FileNotFoundException, IOException {
 		// TODO Auto-generated method stub
 
+		System.out.println("*** Using resolution from Refmac "+RunningParameter.UpdateResolutionRefmac+" ***");
+		
 		File [] ExcelPath=new File(Path).listFiles();
 		
 		for(File Folder : ExcelPath) {
@@ -30,10 +36,29 @@ public static void main(String[] args) throws Exception{
 					
 					ExcelLoader e = new ExcelLoader();
 					Vector<ExcelContents> Container = e.ReadExcel(Excel.getAbsolutePath());
+					
 					for(int i=0 ; i < Container.size() ; ++i) {
 						
-						Container.get(i).Resolution=Container.get(i).PDB_ID.substring(5, 8);
+						if((RunningParameter.UpdateResolutionRefmac.equals("T") && new BigDecimal(RunningParameter.LimitUpdateResolutionFromRefmac).compareTo(new BigDecimal(Container.get(i).Resolution.trim())) == 1 ) || (RunningParameter.UpdateResolutionRefmac.equals("T") && RunningParameter.LimitUpdateResolutionFromRefmac.equals("-1"))) {
+							Container.get(i).Resolution=String.valueOf(new BigDecimal(Container.get(i).Resolution.trim()).setScale(1, RoundingMode.HALF_UP).doubleValue());
+
+						}
+						else {
+							Container.get(i).Resolution=Container.get(i).PDB_ID.substring(5, 8);
+
+						}
+						
 					}
+					//Check if rounded is done or if already rounded 
+					Vector<ExcelContents> OriginalContainer = e.ReadExcel(Excel.getAbsolutePath());
+					//Vector<Boolean> Rounded=new Vector<Boolean>();
+					int Rounded=0;
+					for(int i=0 ; i < Container.size() ; ++i) {
+						if(Container.get(i).Resolution.trim().equals(OriginalContainer.get(i).Resolution.trim()))
+							Rounded++;
+					}
+					if(Rounded==OriginalContainer.size())
+						System.out.println(Excel.getAbsolutePath()+" it seems the resolutions already rounded!");
 					FileUtils.deleteQuietly(Excel);
 					new ExcelSheet().FillInExcel(Container, Excel.getAbsolutePath());
 				}
