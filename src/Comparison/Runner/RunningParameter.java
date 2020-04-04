a186e2f22366947efcc44e0f603448073570e961
Emad Alharbi <emra500@york.ac.uk>
2020-04-04 02:45:34 +0100

    added comments and new parameters

--- src/Comparison/Runner/RunningParameter.java
+++ src/Comparison/Runner/RunningParameter.java
@@ -13,15 +13,15 @@ public class RunningParameter {
 	public static String PDBsDirBuccaneer;// PDBS files that built by Buccaneer
 	public static String castat2Path;// castat2 path
 	public static String CphasesMatchScriptPath;//CphasesMatch Script Path in CCP4 folder
-	public static String RefmacPath;
+	public static String RefmacPath; // path for refmac in bin folder 
 	public static String LogsDirwArp;// logs files for wArp
 	public static String PDBsDirwArp;// PDBS files that built by wArp
 	public static String LogsDirPhenix;// logs files for Phenix
 	public static String PDBsDirPhenix;// PDBS files that built by Phenix
 	public static String PhenixAutobuild;// PhenixAutobuild path
 	public static String BuccaneerPipeLine;
-	public static String wArpAutotracing;
-	public static String ParrtoPhases;
+	public static String wArpAutotracing; // path for ARP/wARP auto_tracing.sh 
+	public static String ParrtoPhases; // columns parrot phases 
 	
 	public static String LogsDir;// logs files for Analyser
 	public static String PDBsDir;// PDBS files for Analyser
@@ -44,39 +44,43 @@ public class RunningParameter {
 	
 	public static String PhenixWorkingDirList; // the txt file which has the working dir lists 
 	public static String PhenixMolProbity;// Path for Mol Probity
-	public static String PDBs;
-	public static String ExcellPath;
+	public static String PDBs; // path for final PDB folder 
+	public static String ExcellPath; // path to folder that contains excel files 
 	
-	public static String Pipelines;
+	public static String Pipelines; // which pipelines need to prepare to run 
 	
 	public static String ChltomDataPath;//Data folder path 
-	public static String ccp4i2Core;
+	public static String ccp4i2Core; 
 	public static String DatafakeAnomalous;
 	
-	public static String IntermediatePDBs;
-	public static String IntermediateLogs;
+	public static String IntermediatePDBs; // path for intermediate PDB folder 
+	public static String IntermediateLogs; // path for intermediate logs folder 
 	
 	public static String CfakeAnom;
 	public static String UsingMolProbity="T"; // Using Mol Probity in the Analyser. Default is False
 	public static String PhasesUsedCPhasesMatch="HLA,HLB,HLC,HLD";// Default is HL  phases.
 	
-	public static String ClusterServerGrid="Slurm";
+	public static String ClusterServerGrid="Slurm"; //  grid engine  
 	public static String ExperimentType="Both";
-	public static String UsingRFree="T";
+	public static String UsingRFree="T"; // T to use R-free in building 
 	
-	public static String BuccaneerIterations="25";
-	public static String PhenixPhases="hltofom.Phi_fom.phi,hltofom.Phi_fom.fom,parrot.ABCD.A,parrot.ABCD.B,parrot.ABCD.C,parrot.ABCD.D";
-	public static String DensityModifiedPhenix="F";
-    public static String PhenixRebuild_in_place="False";
+	public static String BuccaneerIterations="25"; // number of iterations for Buccaneer
+	public static String PhenixPhases="hltofom.Phi_fom.phi,hltofom.Phi_fom.fom,parrot.ABCD.A,parrot.ABCD.B,parrot.ABCD.C,parrot.ABCD.D"; // columns to use for phenix autobuild 
+	public static String DensityModifiedPhenix="F"; // if Parrot phases are used set to T
+    public static String PhenixRebuild_in_place="False"; // enable Rebuild in place for phenix autobuild
     public static String UsingPhenixRebuild_in_place="None"; // set to false or true will effect when run Phenix 
-    public static String Shelxe;
-    public static String SlurmAccount="";
-    public static String SlurmEmail="";
-    public static String CCP4ModuleLoadingCommand="module load chem/ccp4/7.0.066";
-    public static String PhenixLoadingCommand="module load chem/phenix/1.14-3260";
-    public static String ShelxeData;
-    public static String MR="F";
+    public static String Shelxe; // Shelxe binary  file 
+    public static String SlurmAccount=""; // grid engine  account 
+    public static String SlurmEmail=""; // email to use by grid engine to send notification 
+    public static String CCP4ModuleLoadingCommand="module load chem/ccp4/7.0.066"; // load command and name of ccp4 module 
+    public static String PhenixLoadingCommand="module load chem/phenix/1.14-3260";// load command and name of  phenix module
+    public static String ShelxeData; // Path for data folder to use by Shelxe
+    public static String MR="F"; // if this MR cases set to T
     public static String FinalPhasesCPhasesMatch="sfcalc.ABCD.A,sfcalc.ABCD.B,sfcalc.ABCD.C,sfcalc.ABCD.D";// Default is HL  phases.
-    public static String ShelxeReflectionsSpace="-l2";
-    public static String PhenixCluster="F";
+    public static String ShelxeReflectionsSpace="-l2"; // flag to increase reflections space for Shelxe when the reflections are too many  
+    public static String PhenixCluster="F"; // to use cluster feature in Phenix AutobBuild 
+    public static String ReplaceToSingleLetter="F"; // replace pipelines names by its first letter in comparsion tables 
+    public static String UpdateResolutionRefmac="F"; // to use resolution that read from Refmac instead of reading it from the file name (help when the file columns has different resolutions )
+    public static String LimitUpdateResolutionFromRefmac="-1"; // use to limit the resolution from Refmac. Ex  LimitUpdateResolutionFromRefmac=3.1, meaning the resolutions lower than 3.1 read from Refmac and others take from file name
+    public static String RemoveDummyAtomsFromInitialModel="F"; // remove ARP/wARP dummy atoms before use as initial model for others pipelines
 }
