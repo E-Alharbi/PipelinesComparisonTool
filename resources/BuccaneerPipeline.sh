be0556b3bae220daf06ff98fbb97ce6df3a12ff1
Emad Alharbi <emra500@york.ac.uk>
2021-08-09 06:02:23 +0100

    Make more pipelines parameters to be set from the command line

--- resources/BuccaneerPipeline.sh
+++ resources/BuccaneerPipeline.sh
@@ -3,4 +3,4 @@
 #$ -l h_vmem=2G
 #$ -l h_rt=48:00:00
 cd @5
-python @1 -mtzin @2 -seqin  @3 -colin-fo FP,SIGFP -colin-hl parrot.ABCD.A,parrot.ABCD.B,parrot.ABCD.C,parrot.ABCD.D @6 -buccaneer-anisotropy-correction -buccaneer-fast -buccaneer-keyword verbose 5 -cycles @4 @7   
+ @1 -mtzin @2 -seqin  @3 -colin-fo @8 -colin-hl @9 @6 -buccaneer-anisotropy-correction -buccaneer-fast -buccaneer-keyword verbose 5 -cycles @4 @7   
